db95bd6c1f092298eb42732d18281b15b9d64b24
TUNG0922 <tungjinyou@gmail.com>
2024-01-16 21:22:10 +0800

    commit first

--- Project/src/LMS/Borrower.java
+++ Project/src/LMS/Borrower.java
@@ -83,15 +83,15 @@ public class Borrower extends Person
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
         
-        System.out.println(
-                "\nDo you want to update " + getName() + "'s Name ? (y/n)"
-        );  
+        System.out.println("\nDo you want to update " + getName() + "'s Name ? (y/n)");  
         choice = sc.next();
 
         updateBorrowerName(choice, reader);
 
 
-        System.out.println("\nDo you want to update " + getName() + "'s Address ? (y/n)");  
+        System.out.println(
+                "\nDo you want to update " + getName() + "'s Address ? (y/n)"
+        );  
         choice = sc.next();
 
         updateBorrowerAddress(choice, reader);
